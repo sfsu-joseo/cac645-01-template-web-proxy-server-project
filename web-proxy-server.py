458d7ee33ae57df250f78cfd38dedccaf73fb3ea
Jose Ortiz <jortizco@sfsu.edu>
2019-10-06 18:46:31 -0700

    Updated web-proxy-server.py with url redirection to proxy-settings

--- web-proxy-server.py
+++ web-proxy-server.py
@@ -13,5 +13,17 @@ def home():
 def proxy_settings():
     return render_template('proxy-settings.html')
 
+@app.route('/home.html', methods=['POST'])
+
+def get_user_input():
+    url = request.form.get('url')
+    is_private_mode = 0
+    if request.form.get('private'):
+        is_private_mode = 1
+    if "proxy-settings" in url:
+        return proxy_settings()
+    data = {'url': url, 'is_private_mode': is_private_mode}
+    return str(data)
+
 if __name__ == '__main__':
     app.run()
